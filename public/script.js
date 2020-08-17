534cf7aec13ca4233d6f07eaee9c22cf151ce4ab
Bruno <bvrsilva1@gmail.com>
2020-08-17 11:29:42 -0500

    mute and stop video buttons

--- public/script.js
+++ public/script.js
@@ -74,3 +74,45 @@ const scrollToBottom = () => {
   let chatWindow = $('.main__chat_window')
   chatWindow.scrollTop(chatWindow.prop('scrollHeight'))
 }
+
+const setMuteButton = () => {
+  const muteIcon = `<i class='fas fa-microphone'></i><span>Mute</span>`
+  document.querySelector('.main__mute_button').innerHTML = muteIcon
+}
+
+const setUnmuteButton = () => {
+  const unmuteIcon = `<i class='fas fa-microphone-slash'></i><span>Unmute</span>`
+  document.querySelector('.main__stop_video_button').innerHTML = unmuteIcon
+}
+
+const setStopVideoButton = () => {
+  const stopVideoIcon = `<i class="fas fa-video"></i><span>Stop Video</span>`
+  document.querySelector('.main__stop_video_button').innerHTML = stopVideoIcon
+}
+
+const setStartVideoButton = () => {
+  const startVideoIcon = `<i class="fas fa-video-slash"></i><span>Start Video</span>`
+  document.querySelector('.main__stop_video_button').innerHTML = startVideoIcon
+}
+
+const muteUnmute = () => {
+  const enabled = videoStream.getAudioTracks()[0].enabled
+  if (enabled) {
+    videoStream.getAudioTracks()[0].enabled = false
+    setUnmuteButton()
+  } else {
+    videoStream.getAudioTracks()[0].enabled = true
+    setMuteButton()
+  }
+}
+
+const stopStartVideo = () => {
+  const enabled = videoStream.getVideoTracks()[0].enabled
+  if (enabled) {
+    videoStream.getVideoTracks()[0].enabled = false
+    setStartVideoButton()
+  } else {
+    videoStream.getVideoTracks()[0].enabled = true
+    setStopVideoButton()
+  }
+}
